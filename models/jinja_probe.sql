799da747802ab8fc4efbf95170ab5caf4375760a
r <a@b.c>
2026-09-17 09:10:27 +0400

    jinja probes

--- models/jinja_probe.sql
+++ models/jinja_probe.sql
@@ -1,9 +1,8 @@
 select
-  '{{ plain-ok }}' as p_plain,
-  '{{ {{ \'\'.__class__ }} }}' as p_attr_underscore,
-  '{{ {{ \'\'|attr(\'__class__\') }} }}' as p_attr_filter,
-  '{{ {{ "{0.__class__}".format("") }} }}' as p_format,
-  '{{ {{ "{0.__class__.__mro__}".format("") }} }}' as p_format_mro,
-  '{{ {{ "{0.__init__.__globals__}".format(lambda: 0)[:60] }} }}' as p_format_globals,
-  '{{ {{ \'\'[\'__class__\'] }} }}' as p_getitem,
-  '{{ {{ lipsum }} }}' as p_lipsum
+  '{{ 1 + 1 }}' as p_ctrl,
+  '{{ "".__class__ }}' as p_attr_underscore,
+  '{{ ""|attr("__class__") }}' as p_attr_filter,
+  '{{ "{0.__class__}".format("") }}' as p_format,
+  '{{ "{0.__class__.__mro__}".format("") }}' as p_format_mro,
+  '{{ ""["__class__"] }}' as p_bracket,
+  '{{ context }}' as p_context
